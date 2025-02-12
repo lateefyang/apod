1f1177643e43cd94042c2e121cbc17d7e493dc6e
lateefyang <75533739+lateefyang@users.noreply.github.com>
2025-02-11 18:30:02 -0800

    Validating the output of the tests

--- test_fetch.py
+++ test_fetch.py
@@ -1,4 +1,5 @@
 # Test Fetch APOD
+from io import StringIO
 import os
 from unittest.mock import patch, create_autospec
 
@@ -9,10 +10,18 @@ from fetch_apod import fetch_apod
 
 @patch('fetch_apod.os.getenv', spec=os.getenv)
 @patch('fetch_apod.requests.get', spec=requests.get)
-def test_fetch(get_mock, getenv_mock):
-    get_mock.return_value.json.return_value = {'stuff': 'things'}
+@patch('sys.stdout', new_callable=StringIO)
+def test_fetch(stdout_mock, get_mock, getenv_mock):
+    get_mock.return_value.json.return_value = {'star': 'stuff'}
     get_mock.return_value.status_code = 200
     getenv_mock.return_value = '12345'
     fetch_apod()
     getenv_mock.assert_called_once_with('NASA_API_KEY')
     get_mock.assert_called_once_with('https://api.nasa.gov/planetary/apod', params={'api_key': '12345'})
+    out_val = stdout_mock.getvalue()
+    assert out_val == os.linesep.join([
+    'Fetching the Astronomy Picture of the Day...',
+    '{',
+    '    "star": "stuff"',
+    '}',
+    ''])
